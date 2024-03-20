ac551591f67b832a86d77d9e704223c0f8559cde
Camilo <camilorte1030@gmail.com>
2024-03-20 00:46:58 -0500

    Se creó modelo menú

--- food/models.py
+++ food/models.py
@@ -4,5 +4,8 @@ from django.contrib.auth.models import User
 class UserProfile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
 
-
+class Menu(models.Model):
+    nombre = models.CharField(max_length=100)
+    descripcion = models.CharField(max_length=250)
+    imagen = models.ImageField(upload_to='menu/images/')
 
