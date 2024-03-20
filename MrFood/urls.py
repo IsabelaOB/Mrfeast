fe401a0d75e2da04e2a0d460ad42e7b05fe1141e
RojaMonseny <osorioboteroisabela@gmail.com>
2024-03-19 21:48:30 -0500

    urls_nuevas

--- MrFood/urls.py
+++ MrFood/urls.py
@@ -1,4 +1,3 @@
-
 from django.contrib import admin
 from django.urls import path
 from food import views as foodViews
@@ -6,8 +5,8 @@ from django.views.generic import TemplateView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', foodViews.login_view, name='login'),
+    path('', TemplateView.as_view(template_name='inicio.html'), name='inicio'),
+    path('login/', foodViews.login_view, name='login'),
     path('nombre_de_la_url/', foodViews.home_view, name='nombre_de_la_url'),
     path('nombre_de_la/', TemplateView.as_view(template_name='page.html'), name='nombre_de_la'),
-    # Puedes agregar más URLs según tus necesidades
 ]
