e7ca16ec49f13e864ae3f3d1f3561d63ac35f948
Camilo <camilorte1030@gmail.com>
2024-04-24 02:53:11 -0500

    arreglado diseño y agregado logo

--- food/models.py
+++ food/models.py
@@ -8,6 +8,7 @@ class Menu(models.Model):
     nombre = models.CharField(max_length=100)
     descripcion = models.CharField(max_length=250)
     imagen = models.ImageField(upload_to='menu/images/')
+    link = models.URLField(null=True, blank=True)
     
     def __str__(self):
         return self.nombre
